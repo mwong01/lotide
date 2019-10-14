22d732475918855c98d355c8f12b0aa4fe112bff
mwong01 <mattw90@gmail.com>
2019-10-14 20:38:23 +0000

    implementing head to retrieve first array element

--- head.js
+++ head.js
@@ -6,10 +6,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function (actual) {
+const head = function(actual) {
   return actual[0];
-}
-
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
