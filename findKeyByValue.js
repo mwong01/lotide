4b204ec42f4d0ba935b6361d1073db297505149d
mwong01 <mattw90@gmail.com>
2019-10-16 19:22:25 +0000

    checking tv show genre using tv shows

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  for (const objKeys in obj ) {
+    if (obj[objKeys] === value) {
+      return objKeys;
+    }
+  }
+};
+
+findKeyByValue();
+
+// TEST CODE
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "sci-fi");
