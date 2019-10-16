a21844b5ed6323ac0ed4018d3185ae1123972e94
mwong01 <mattw90@gmail.com>
2019-10-16 17:09:00 +0000

    count number of letters in a given string

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(str) {
+  let count = {};
+  console.log(str);
+  let strNew = str.split(" ").join("").toLowerCase();
+  for (let i = 0; i < strNew.length; i++) {
+    let char = strNew.charAt(i);
+    if (count[char]) {
+      count[char]++;
+    } else {
+      count[char] = 1;
+    }
+  }
+  return count;
+};
+
+// TEST CODE
+console.log(assertEqual(countLetters("lighthouse in the house")["l"], 1));
+console.log(assertEqual(countLetters("lighthouse in the house")["g"], 3));
